52e466c52260cb53d5f4c48fc59f3af2e3741adb
Daniel Austin <121881138+IMDANIELAUSTIN@users.noreply.github.com>
2026-03-15 11:25:07 -0700

    Fix import path for projects data

--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { projects } from "/src/data/projects";
+import { projects } from "./data/projects";
 
 type SortMode = "popular" | "az" | "newest";
 
