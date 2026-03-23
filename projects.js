af48df1f6ff8dee8cd93700f7ae39bb3cd3b1259
Daniel Austin <me@imdanielaustin.com>
2026-03-22 18:44:32 -0700

    Update projects.js

--- projects.js
+++ projects.js
@@ -43,7 +43,7 @@ const projects = [
       { type: 'image', url: 'assets/snap9.png' },
       { type: 'image', url: 'assets/snapsponsor.png', bgColor: '#ffffff', },
       { type: 'embed', html: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/684013951?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Snap Academies 2021"></iframe></div>' },
-      { type: 'button', text: 'View Project Presentation Here', url: 'https://youtu.be/IZzVvmqM5J8?si=LHs5kluTgvQakrAG', bgColor: '#fffc00', color: '#ffffff' },
+      { type: 'button', text: 'View Project Presentation Video Here', url: 'https://youtu.be/IZzVvmqM5J8?si=LHs5kluTgvQakrAG', bgColor: '#fffc00', color: '#000000' },
     ]
   },
   {
