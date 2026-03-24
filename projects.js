f6278c999428f3c1afad102f5ca97d50784aee0e
Daniel Austin <121881138+IMDANIELAUSTIN@users.noreply.github.com>
2026-03-24 13:50:35 -0700

    Update projects.js

--- projects.js
+++ projects.js
@@ -9,7 +9,7 @@ const projects = [
     blocks: [
       { type: 'text', content: "NT Window Inc. is a leading manufacturer of windows and doors, serving both residential and commercial markets. In 2025, I was commissioned to refresh the NT Window brand guidelines to prepare for the company's 35th anniversary while extending the celebration to automotive and employee apparel." },
       { type: 'image', url: 'assets/ntwindow1.png' },
-      { type: 'text', content: 'NT Window needed a brand identity that communicated innovation and reliability. The resulting mark uses bold geometric forms to convey strength and forward momentum, creating an immediately recognizable symbol. In order to make the identity both functional and memorable, the design was streamlined to its essential elements. The color palette and typography were carefully selected to ensure versatility across digital and print applications.' },
+      { type: 'text', content: 'As NT Window celebrated their 35th anniversary they also needed their brand identity to communicate reliability while extending the brand to other mediums. With direction from the in-house Marketing Team, I was able to develop an Anniversary Decal to be used on the automotive fleet and then further take these refinements to develope an updated brand guidelines and packaging design.' },
       { type: 'image', url: 'assets/ntwindow2.png' },
       { type: 'image', url: 'assets/ntwindow3.png' },
       { type: 'image', url: 'assets/ntwindow4.png' },
@@ -29,7 +29,7 @@ const projects = [
     blocks: [
       { type: 'text', content: "Selected as a Snap Design Academy Scholar, I joined a cohort of emerging designers for an immersive, industry-level program hosted by Snap Inc and LA-Tech. Working cross-functionally with designers, engineers, and storytellers, I helped concept and develop the UI/UX for a new Snap feature designed to elevate underrepresented Los Angeles Neighborhoods, gaining hands-on experience in design research, direct mentorship, and critical guidance from Snap's creative team." },
       { type: 'image', url: 'assets/snap1.png', bgColor: '#fffc00', },
-      { type: 'text', content: "Snapchat Design Academy identity system was developed to represent unity and collective purpose. The mark draws on symbolic elements that resonate across cultures, creating a universal visual language. The identity system extends beyond the logo to encompass a comprehensive toolkit of patterns, icons, and typography that allow for flexible yet consistent brand expression. The resulting system has proven highly adaptable, serving the organization's diverse communication needs across events, publications, and digital platforms." },
+      { type: 'text', content: "Using research provided by Los Angeles Neighborhood Land Trust, my team and I were able to address the brief of, addressing inaccessibility of green spaces within Los Angeles urban neighborhoods. I was in charge of the overall Interface Design making sure our product aligned with Snap's established guidelines while still offering visual solutions for problems like, in-app Web View." },
       { type: 'image', url: 'assets/snapbento.png', bgColor: '#ffffff', },
       { type: 'embed', html: '<iframe width="100%" style="aspect-ratio: 16/9;" src="https://www.youtube-nocookie.com/embed/dsGMxnMAXQs?si=VVsThF7PuiOv9b4X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>' },
       { type: 'image', url: 'assets/snap2.png', bgColor: '#ffffff', },
@@ -55,8 +55,6 @@ const projects = [
     blocks: [
       { type: 'text', content: "A strategic rebrand for Artbound, an Emmy® award-winning arts and culture series documenting creative innovation across Southern California. This project repositions the identity to better reflect its role as both a cultural archive and contemporary media platform, balancing editorial credibility with visual impact." },
       { type: 'image', url: 'assets/artbound1.png' },
-      { type: 'text', content: "The PBS (Public Broadcasting Service) and Artbound mark was developed to represent congruence between different mediums of art and media in an ever-connected world. This system encompasses a scalable logomark and typemark that can be used across a variety of applications and with both a horizontal and vertical lockups. The result proves to be an adaptable solution while serving for the sub-brand's communication needs across events and digital platforms while maintaining an ability to attach to PBS branding." },
-      { type: 'quote', text: '“Insert quote from someone at PBS”', author: 'Daniel Lee Austin, Designer', bgColor: '#171A60', color: '#ECF6FB' },
       { type: 'image', url: 'assets/artbound2.png', bgColor: '#ECF6FB', },
       { type: 'image', url: 'assets/artbound3.png', bgColor: '#ECF6FB', },
     ]
@@ -70,7 +68,7 @@ const projects = [
     blocks: [
       { type: 'text', content: "In Spring 2021, I participated in Community Studio, a transdisciplinary course at ArtCenter College of Design structured as a real design consultancy. Working alongside students from multiple disciplines, I contributed to a comprehensive brand and design overhaul for Sole Folks, a Black-owned retail co-op in Leimert Park, Los Angeles. The project spanned brand identity, graphic design, and retail experience delivering professional-grade work directly to a real client and community." },
       { type: 'image', url: 'assets/solefolks1.png', caption: 'Photo by Jason Armond, featured on Los Angeles Times' },
-      { type: 'text', content: "Sole Folks identity system was developed to represent unity and collective purpose. The mark draws on symbolic elements that resonate across cultures, creating a universal visual language. The identity system extends beyond the logo to encompass a comprehensive toolkit of patterns, icons, and typography that allow for flexible yet consistent brand expression. The resulting system has proven highly adaptable, serving the organization's diverse communication needs across events, publications, and digital platforms." },
+      { type: 'text', content: "Sole Folks identity system was mostly developed before our studio became involved. Our work was to further refine the brand while I was in charge of creating a brand pattern, developement of Brand Guidelines, and layout designs for presentations. The mark draws on symbolic elements that resonate across cultures and I took that fooundation to juxtapose it with the concept of designs found on the soles of a shoe and broken chains. The result is a system that is adaptable while serving the organization's diverse communication needs across events, publications, and digital platforms. The pattern can currently be seen on the Front Facade of Sole Folks at Leimert Park." },
       { type: 'image', url: 'assets/solefolks2.png' },
       { type: 'image', url: 'assets/solefolks3.png' },
       { type: 'image', url: 'assets/solefolks4.png' },
@@ -90,7 +88,7 @@ const projects = [
       { type: 'image', url: 'assets/akkurat1.png' },
       { type: 'text', content: "Using Akkurat to develop a typographic system juxtaposed with the works of Alfred Hitchcock, I explor Akkurat's role as a contemporary neo-grotesque in editorial design. The booklet examines how neutrality can be leveraged as a design tool—using structure, scale, and rhythm to create clarity without sacrificing visual identity., exploring its role as a contemporary neo-grotesque in editorial design. This booklet examines how neutrality can be leveraged as a design tool—using structure, scale, and rhythm to create clarity without sacrificing visual identity." },
       { type: 'image', url: 'assets/akkurat2.png' },
-      { type: 'text', content: "Objective: Design an 8-page editorial booklet that communicates the unique characteristics, history, and functional versatility using the typeface Akkurat while maintaining a cohesive visual system." },
+      { type: 'text', content: "Objective: Design an 8-page editorial booklet that communicates the unique characteristics, history, and functional versatility of the typeface Akkurat." },
       { type: 'image', url: 'assets/akkurat3.png' },
       { type: 'image', url: 'assets/akkurat4.png' },
       { type: 'image', url: 'assets/akkurat5.png' },
