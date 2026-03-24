ff3f295db841b313a964bc45a27daf60be18e6c9
Daniel Austin <me@imdanielaustin.com>
2026-03-24 08:42:31 -0700

    ,

--- projects.js
+++ projects.js
@@ -89,7 +89,7 @@ const projects = [
       { type: 'image', url: 'assets/akkurat3.png' },
       { type: 'image', url: 'assets/akkurat4.png' },
       { type: 'image', url: 'assets/akkurat5.png' },
-      { type: 'gif', url: 'assets/akkurat mograph.webm' },
+      { type: 'video', url: 'assets/akkurat mograph.webm' },
 
     ]
   },
