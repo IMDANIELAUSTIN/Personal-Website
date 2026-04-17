fe46d38777cdd54a53acb715758ba0a2a71919fe
Daniel Austin <me@imdanielaustin.com>
2026-04-17 01:50:06 -0700

    Update projects.js

--- projects.js
+++ projects.js
@@ -35,7 +35,7 @@ const projects = [
   },
     {
     slug: "thebitcoinlayer",
-    image: "assets/TBL Full Logomark Hero Animation.gif",
+    image: "assets/TBL Typemark Redline Animation.gif",
     name: "The Bitcoin Layer — Identity Refinement",
     type: "Brand Design, Visual Design",
     year: 2023,
